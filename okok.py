331649fde97db3ca211711a76f319d3bcfc14117
NgocPHuc <nguyenphucmcohi1234@gmail.com>
2025-04-26 10:54:57 +0700

    bankssmth

--- okok.py
+++ okok.py
@@ -2,6 +2,7 @@ import requests
 import time
 from gtts import gTTS
 import os
+from playsound import playsound
 imurl = "https://manhteky123-banking.hf.space/currentCK"
 DLCK = requests.get(imurl)
 SLCK = DLCK.json()['currentCK']
@@ -19,5 +20,5 @@ while True:
         red = "bạn đã nhận được" + str(DLCK.json()['transferAmount']) + "đồng"
         tts = gTTS(text=red, lang='vi')
         tts.save("mone.mp3")
-        os.system("start mone.mp3")
+        playsound("mone.mp3")
         SLCK += 1
